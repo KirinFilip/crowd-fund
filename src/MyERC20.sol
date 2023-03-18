2ed5e4cea63c3d0eb9ed9cc968cc62a62e2a252a
KirinFilip <filip.kirin.zg1@gmail.com>
2023-03-18 14:34:57 +0100

    for tests

--- src/MyERC20.sol
+++ src/MyERC20.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
 contract MyERC20 is ERC20 {
-    constructor() ERC20("Name", "SYM") {
-        this;
+    constructor() ERC20("MyERC20", "ERC20") {
+        // _mint(msg.sender, initialSupply);
     }
 }
